c0b8e158984d8a60560c554b7cc4e6cb2391045b
Appolia <VasyGrenkin@gmail.com>
2022-07-29 10:00:34 +0300

    fix

--- src/Main.java
+++ src/Main.java
@@ -13,29 +13,29 @@ public class Main {
             }
         }
         System.out.println(" ");
-        double[] q = {1.57, 7.654, 9.986};
-        for (int t = 0; t < q.length; t++) {
-            if (t == q.length - 1)
-                System.out.print(q[t]);
+        double[] fractions = {1.57, 7.654, 9.986};
+        for (int i = 0; i < fractions.length; i++) {
+            if (i == fractions.length - 1)
+                System.out.print(fractions[i]);
             else {
-                System.out.print(q[t] + ", ");
+                System.out.print(fractions[i] + ", ");
             }
         }
-        System.out.println(" ");
+        System.out.println();
         int[] breakTime = new int[4];
         breakTime[0] = 10;
         breakTime[1] = 12;
         breakTime[2] = 14;
         breakTime[3] = 16;
-        for (int r = 0; r < breakTime.length; r++) {
-            if (r == breakTime.length - 1)
-                System.out.print("перерыв в " + breakTime[r] + " часов.");
+        for (int i = 0; i < breakTime.length; i++) {
+            if (i == breakTime.length - 1)
+                System.out.print("перерыв в " + breakTime[i] + " часов.");
             else {
-                System.out.print("перерыв в " + breakTime[r] + " часов, ");
+                System.out.print("перерыв в " + breakTime[i] + " часов, ");
             }
         }
         //ex3
-        System.out.println("");
+        System.out.println();
         for (int i = numbers.length - 1; i >= 0; i--) {
             if (i == numbers.length - 3)
                 System.out.print(numbers[i]);
@@ -44,20 +44,20 @@ public class Main {
             }
         }
         System.out.println("");
-        for (int t = q.length - 1; t >= 0; t--) {
-            if (t == q.length - 3)
-                System.out.print(q[t]);
+        for (int i = fractions.length - 1; i >= 0; i--) {
+            if (i == fractions.length - 3)
+                System.out.print(fractions[i]);
             else {
-                System.out.print(q[t] + ", ");
+                System.out.print(fractions[i] + ", ");
             }
         }
         System.out.println("");
 
-        for (int r = breakTime.length - 1; r >= 0; r--) {
-            if (r == breakTime.length - 4)
-                System.out.print("перерыв в " + breakTime[r] + " часов.");
+        for (int i = breakTime.length - 1; i >= 0; i--) {
+            if (i == breakTime.length - 4)
+                System.out.print("перерыв в " + breakTime[i] + " часов. ");
             else {
-                System.out.print("перерыв в " + breakTime[r] + " часов, ");
+                System.out.print("перерыв в " + breakTime[i] + " часов, ");
             }
         }
         //ex4
@@ -66,12 +66,12 @@ public class Main {
         number[0] = 1;
         number[1] = 2;
         number[2] = 3;
-        int z = 1;
+        int plusOne = 1;
         for (int i = 0; i < number.length; i++) {
             if (number[i] % 2 != 0 && i == numbers.length - 1)
-                System.out.print(number[i] + z);
+                System.out.print(number[i] + plusOne);
             else if (number[i] % 2 != 0)
-                System.out.print(number[i] + z + ", ");
+                System.out.print(number[i] + plusOne + ", ");
             else {
                 System.out.print(number[i] + ", ");
             }
